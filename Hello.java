681edd2092f2839a71dc6173d11cffb99c9d2b56
smarts8855 <olaoyea64@gmail.com>
2025-01-07 12:22:27 +0100

    first java

---
+++ Hello.java
@@ -0,0 +1,10 @@
+class Hello
+{
+    public static void main(String a[] )
+    {
+        int num1 = 3;
+        int num2 = 3;
+        int result = num1 + num2;
+        System.out.println(result);
+    }
+}
