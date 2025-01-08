ba5687dfcca010458a0499ffd00f6edc77999d73
smarts8855 <olaoyea64@gmail.com>
2025-01-08 11:14:34 +0100

    operator & conditional statement

--- Hello.java
+++ Hello.java
@@ -1,10 +1,23 @@
 class Hello
 {
-    public static void main(String a[] )
+    public static void main(String args[] )
     {
-        int num1 = 3;
-        int num2 = 3;
-        int result = num1 + num2;
-        System.out.println(result);
+        // byte b = 127;
+        int x = 7;
+        int y = 16;
+        int z = 25;
+
+        // int result = num1 % num2;
+
+        // num1 = num1 + 2;
+        // num1 *= 16;
+        // int result = num1--;
+        // boolean result = x < y ;
+        if (x > y && x>z) 
+           System.out.println(x);
+        else if(y>x && y>z)
+           System.out.println(y);
+        else 
+           System.out.println(z);
     }
 }
